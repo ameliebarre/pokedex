a28940273602fe9d0891594577a5d70881a3368b
Amélie Barré <amliebarre@gmail.com>
2018-11-25 12:12:53 +0100

    initialize secure and public components for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { PublicComponent } from './layouts/public/public.component';
+import { SecureComponent } from './layouts/secure/secure.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PublicComponent,
+    SecureComponent
   ],
   imports: [
     BrowserModule,
