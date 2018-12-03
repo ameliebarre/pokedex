fa30219ab9f97161cba82379a15eaab5a1b24a6e
Amélie Barré <amliebarre@gmail.com>
2018-12-03 14:17:01 +0100

    testing register form

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -2,38 +2,67 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {JwtHelperService} from '@auth0/angular-jwt';
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+import { ToastrModule } from 'ng6-toastr-notifications';
+import { RouterTestingModule } from '@angular/router/testing';
+import {AuthService} from '../../shared/services/auth.service';
 
 fdescribe('RegisterComponent', () => {
-  let component: RegisterComponent;
+  let comp: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-
+  let authService: AuthService;
   const formBuilder: FormBuilder = new FormBuilder();
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ RegisterComponent ],
       imports: [
+        JwtModule.forRoot({
+          config: {
+            tokenGetter: () => {
+              return '';
+            }
+          }
+        }),
         ReactiveFormsModule,
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        ToastrModule.forRoot(),
+        RouterTestingModule
       ],
+      providers: [
+        JwtHelperService
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
+    comp = fixture.componentInstance;
+
+    authService = fixture.debugElement.injector.get(AuthService);
 
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(comp).toBeTruthy();
   });
 
   it('should register user', () => {
+    comp.registerForm.controls['name'].setValue('John Smith');
+    comp.registerForm.controls['email'].setValue('john.smith@test.com');
+    comp.registerForm.controls['password'].setValue('xylHWGUyvd');
+    comp.registerForm.controls['confirmPassword'].setValue('xylHWGUyvd');
+
+    spyOn(authService, 'register').and.callThrough();
+
+    comp.onSubmit();
+
+    expect(comp.registerForm.errors).toBeFalsy();
+    expect(authService.register).toHaveBeenCalled();
   });
 });
