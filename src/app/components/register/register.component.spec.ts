db3c2a3b7ce865befb01ae7aba9217bed4b44c65
Amélie Barré <amliebarre@gmail.com>
2018-12-04 13:56:59 +0100

    registerForm tests name validity and submit method

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -1,19 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Observable } from 'rxjs';
 import { RegisterComponent } from './register.component';
-import { FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
 import { ToastrModule } from 'ng6-toastr-notifications';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AuthService } from '../../shared/services/auth.service';
 
 fdescribe('RegisterComponent', () => {
   let comp: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
   let authService: AuthService;
-  const formBuilder: FormBuilder = new FormBuilder();
 
   beforeEach(async(() => {
 
@@ -57,9 +55,24 @@ fdescribe('RegisterComponent', () => {
     expect(comp.registerForm.valid).toBeFalsy();
   });
 
+  it('should test name field validity', () => {
+    let errors = {};
+    const name = comp.registerForm.controls['name'];
+    expect(name.valid).toBeFalsy();
+
+    // Name field is required
+    errors = name.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    // Set name with value
+    name.setValue('John Smith');
+    errors = name.errors || {};
+    expect(errors['required']).toBeFalsy();
+  });
+
   it('should test email field validity', () => {
     let errors = {};
-    let email = comp.registerForm.controls['email'];
+    const email = comp.registerForm.controls['email'];
     expect(email.valid).toBeFalsy();
 
     // Email field is required
@@ -78,9 +91,9 @@ fdescribe('RegisterComponent', () => {
     expect(errors['pattern']).toBeFalsy();
   });
 
-  it('should test password field validity',() => {
+  it('should test password field validity', () => {
     let errors = {};
-    let password = comp.registerForm.controls['password'];
+    const password = comp.registerForm.controls['password'];
     expect(password.valid).toBeFalsy();
 
     // Password field is required
@@ -94,20 +107,19 @@ fdescribe('RegisterComponent', () => {
   });
 
   it('should register user', () => {
+
+    expect(comp.registerForm.valid).toBeFalsy();
+
     comp.registerForm.controls['name'].setValue('John Smith');
     comp.registerForm.controls['email'].setValue('john.smith@test.com');
     comp.registerForm.controls['password'].setValue('xylHWGUyvd');
     comp.registerForm.controls['confirmPassword'].setValue('xylHWGUyvd');
 
+    spyOn(authService, 'register').and.callThrough();
+
     comp.onSubmit();
 
-    console.log(comp.registerForm);
-
-    // expect(comp.registerForm.invalid).toBeTruthy();
-  });
-
-  it('should return an error if passwords are the same', () => {
-    // comp.registerForm.controls['password'].setValue('xylHWGUyvd');
-    // comp.registerForm.controls['confirmPassword'].setValue('xylHWGUyvd');
+    expect(comp.registerForm.valid).toBeTruthy();
+    expect(authService.register).toHaveBeenCalled();
   });
 });
