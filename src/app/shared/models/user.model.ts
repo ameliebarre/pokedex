7a3ab1e2182dfe580e777beb275975a6b027bc84
Amélie Barré <amliebarre@gmail.com>
2019-07-30 13:57:29 +0200

    display username instead of name

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,13 +1,13 @@
 export class User {
   id: number;
-  name: string;
+  username: string;
   password: string;
   email: string;
   token: string;
 
   constructor(obj?: any) {
     this.id = obj && Number(obj.id) || null;
-    this.name = obj && obj.name || null;
+    this.username = obj && obj.name || null;
     this.password = obj && obj.password || null;
     this.email = obj && obj.email || null;
     this.token = obj && obj.token || null;
