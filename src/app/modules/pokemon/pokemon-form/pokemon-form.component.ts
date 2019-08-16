0b9230df45a59f34e9588892742a558658979a36
Amélie Barré <amliebarre@gmail.com>
2019-08-16 15:10:11 +0200

    pokemon form : choose generation

--- src/app/modules/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/modules/pokemon/pokemon-form/pokemon-form.component.ts
@@ -16,7 +16,8 @@ export class PokemonFormComponent implements OnInit {
 
   pokemonForm: FormGroup;
   isExists = false;
-  isInexistant = false;
+  dropdownGenerations = [];
+  dropdownGenerationsSettings = {};
 
   constructor(
     private pokemonService: PokemonService,
@@ -25,6 +26,23 @@ export class PokemonFormComponent implements OnInit {
 
   ngOnInit() {
     this.setPokemonForm();
+
+    this.dropdownGenerations = [
+      { item_id: 1, item_text: 'Première' },
+      { item_id: 2, item_text: 'Deuxième' },
+      { item_id: 3, item_text: 'Troisième' },
+      { item_id: 4, item_text: 'Quatrième' },
+      { item_id: 5, item_text: 'Cinquième' }
+    ];
+
+    this.dropdownGenerationsSettings = {
+      singleSelection: true,
+      placeholder: 'Choisir la génération du Pokemon',
+      idField: 'item_id',
+      textField: 'item_text',
+      allowSearchFilter: false,
+      enableCheckAll: false
+    };
   }
 
   setPokemonForm() {
@@ -32,24 +50,17 @@ export class PokemonFormComponent implements OnInit {
       id: [this.pokemon._id],
       name: [this.pokemon.national_number],
       slug: [this.pokemon.slug],
-      numbers: this.fb.group({
-        national_number: [this.pokemon.national_number],
-        kanto_number: [this.pokemon.kanto_number],
-        johto_number: [this.pokemon.johto_number],
-        hoenn_number: [this.pokemon.hoenn_number],
-        sinnoh_number: [this.pokemon.sinnoh_number],
-        unys_number: [this.pokemon.unys_number],
-        kalos_number: [this.pokemon.kalos_number],
-        alola_number: [this.pokemon.alola_number],
-      }),
-      family: [this.pokemon.family]
+      family: [this.pokemon.family],
+      generation: [this.pokemon.generation],
     });
   }
 
-  get numbers() {
-    return this.pokemonForm.controls.numbers as FormGroup;
-  }
-
+  /**
+   * Set the value for the slug based on the name input
+   * Check if the Pokemon already exists. If it does, display an error message.
+   *
+   * @param event
+   */
   bindNameValue(event: any) {
     const pokemonName = event.target.value;
     this.pokemonForm.get('slug').setValue(pokemonName.toLowerCase());
@@ -64,10 +75,23 @@ export class PokemonFormComponent implements OnInit {
     );
   }
 
+  /**
+   * Remove the text in the input name
+   * (only if the Pokemon already exists)
+   */
   removeName() {
     this.pokemonForm.get('name').setValue('');
     this.pokemonForm.get('slug').setValue('');
     this.isExists = false;
+  }
+
+  /**
+   * Choose the Pokemon's generation
+   * @param {{item_id: number, item_text: string}} event
+   */
+  onGenerationSelect(event: { item_id: number, item_text: string }) {
+    this.pokemonForm.get('generation').setValue(event.item_id);
+    console.log(this.pokemonForm.value);
   }
 
   savePokemon() { }
