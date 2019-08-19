03fd12a31b4a7c790efe1ab39927fb40e105a9bc
Amélie Barré <amliebarre@gmail.com>
2019-08-19 15:47:46 +0200

    create route to pokemon-view component

--- src/app/modules/pokemon/pokemon-routing.module.ts
+++ src/app/modules/pokemon/pokemon-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AddPokemonComponent } from './add-pokemon/add-pokemon.component';
+import { PokemonViewComponent } from './pokemon-view/pokemon-view.component';
 
 const routes: Routes = [
   {
     path: 'add',
     component: AddPokemonComponent
+  },
+  {
+    path: ':id',
+    component: PokemonViewComponent
   }
 ];
 
