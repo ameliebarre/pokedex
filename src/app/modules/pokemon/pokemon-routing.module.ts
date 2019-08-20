d6ab149897c4a956874de97d08bfde9a46a70618
Amélie Barré <amliebarre@gmail.com>
2019-08-20 23:04:35 +0200

    create path to edit a Pokemon

--- src/app/modules/pokemon/pokemon-routing.module.ts
+++ src/app/modules/pokemon/pokemon-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AddPokemonComponent } from './add-pokemon/add-pokemon.component';
 import { PokemonViewComponent } from './pokemon-view/pokemon-view.component';
+import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 
 const routes: Routes = [
   {
     path: 'add',
     component: AddPokemonComponent
+  },
+  {
+    path: ':id/edit',
+    component: EditPokemonComponent
   },
   {
     path: ':id',
