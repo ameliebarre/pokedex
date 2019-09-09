fc7a21c830c17bae5d22e38e4191fd8f95eb219d
Amélie Barré <amliebarre@gmail.com>
2019-09-10 00:12:12 +0200

    create next button and show the next Pokemon

--- src/app/modules/pokemon/pokemon-view/pokemon-view.component.ts
+++ src/app/modules/pokemon/pokemon-view/pokemon-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { PokemonService } from '../../../shared/services/pokemon.service';
+import { Pokemon } from '../../../shared/models/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-view',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonViewComponent implements OnInit {
 
-  constructor() { }
+  pokemon: Pokemon;
+  prevPokemon: Pokemon;
+  nextPokemon: Pokemon;
+  showPrev: boolean;
+  showNext: boolean;
+
+  constructor(
+    public pokemonService: PokemonService,
+    public route: ActivatedRoute,
+    public router: Router
+  ) { }
 
   ngOnInit() {
+
+    this.showPrev = false;
+    this.showNext = false;
+
+    const slug = this.route.snapshot.params['slug'];
+
+    this.pokemonService.getPokemonBySlug(slug).subscribe(
+      (pokemon: Pokemon) => {
+        this.pokemon = pokemon;
+
+        if (this.pokemon.next !== null) {
+          this.nextPokemon = this.pokemon.next;
+        }
+
+        if (this.pokemon.prev !== null) {
+          this.prevPokemon = this.pokemon.prev;
+        }
+      }
+    );
   }
 
+  showNextPokemon() {
+    this.showNext = true;
+  }
+
+  hideNextPokemon() {
+    this.showNext = false;
+  }
+
+  showPreviousPokemon() {
+    this.showPrev = true;
+  }
+
+  hidePreviousPokemon() {
+    this.showPrev = false;
+  }
+
+  viewPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/pokemons', pokemon.slug]);
+  }
 }
