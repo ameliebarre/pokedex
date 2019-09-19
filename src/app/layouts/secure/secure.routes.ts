85b925e254ae99c038f197cf17d4a43c2f0c69ce
Amélie Barré <amliebarre@gmail.com>
2019-09-19 16:28:17 +0200

    fix Routes error

--- src/app/layouts/secure/secure.routes.ts
+++ src/app/layouts/secure/secure.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes} from '@angular/router';
 import { HomeComponent } from '../../components/home/home.component';
 
 export const SECURE_ROUTES: Routes = [
