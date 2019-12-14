ec5ca9737c7a472b5d4322fbc629d9f573825409
Amélie Barré <amliebarre@gmail.com>
2019-12-14 23:54:57 +0100

    Revert "pokemon view : create tabs system"
    This reverts commit c6a3f50f9eb18f79d1828f6e20754f9cad9b053b.

--- src/app/modules/pokemon/pokemon.module.ts
+++ src/app/modules/pokemon/pokemon.module.ts
@@ -10,8 +10,6 @@ import { PokemonViewComponent } from './pokemon-view/pokemon-view.component';
 import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 import {TypesModalComponent} from '../../components/types-modal/types-modal.component';
 import { PokemonFiltersComponent } from './pokemon-filters/pokemon-filters.component';
-import { TabsComponent } from './pokemon-tabs/tabs/tabs.component';
-import { TabComponent } from './pokemon-tabs/tab/tab.component';
 
 @NgModule({
   declarations: [
@@ -21,9 +19,7 @@ import { TabComponent } from './pokemon-tabs/tab/tab.component';
     PokemonViewComponent,
     EditPokemonComponent,
     TypesModalComponent,
-    PokemonFiltersComponent,
-    TabsComponent,
-    TabComponent
+    PokemonFiltersComponent
   ],
   imports: [
     PokemonRoutingModule,
